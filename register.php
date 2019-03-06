8b768298e56f53cbd50662cb45893138e2b198e3
Reinhard Lackner <pasta.cool@nerei.net>
2019-03-06 23:15:57 +0100

    removed styling

--- register.php
+++ register.php
@@ -104,25 +104,24 @@
     <h2>Sign Up</h2>
     <p>Please fill this form to create an account.</p>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+        <div>
             <label>Username</label>
-            <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-            <span class="help-block"><?php echo $username_err; ?></span>
-        </div>
-        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <input type="text" name="username" value="<?php echo $username; ?>">
+            <span><?php echo $username_err; ?></span>
+
+
             <label>Password</label>
-            <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-            <span class="help-block"><?php echo $password_err; ?></span>
-        </div>
-        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <input type="password" name="password" value="<?php echo $password; ?>">
+            <span><?php echo $password_err; ?></span>
+
+
             <label>Confirm Password</label>
-            <input type="password" name="confirm_password" class="form-control"
-                value="<?php echo $confirm_password; ?>">
-            <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            <input type="password" name="confirm_password" value="<?php echo $confirm_password; ?>">
+            <span><?php echo $confirm_password_err; ?></span>
         </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Submit">
-            <input type="reset" class="btn btn-default" value="Reset">
+        <div>
+            <input type="submit" value="Submit">
+            <input type="reset" value="Reset">
         </div>
         <p>Already have an account? <a href="login.php">Login here</a>.</p>
     </form>
